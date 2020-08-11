ec80c29a1c2241d46f01901396a5673a6d102aa9
kuntalghosh <kuntalghosh.ewu@gmail.com>
2020-08-11 16:07:48 +0600

    uploading my project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '730442ac84223e717eb565a16431da9e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0523de42-b3d4-46fa-84b2-8de67d4c0946'
+};
